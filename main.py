c5c59c1401de6b9d0419da4d7f2cf3f08ca80e1c
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2024-02-12 14:40:49 -0500

    added data validation pipeline

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from rk_the_bartender import logger
 from rk_the_bartender.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from rk_the_bartender.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+
+STAGE_NAME = "Data Ingestion"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
---
+++ src/rk_the_bartender/components/data_validation.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from rk_the_bartender import logger
+from rk_the_bartender.config.configuration import DataValidationConfig
+
+
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+
+    def validate_all_columns(self)-> bool:
+        try:
+            validation_status = None
+
+            data = pd.read_csv(self.config.unzip_data_dir)
+            all_cols = list(data.columns)
+
+            all_schema = self.config.all_schema.keys()
+
+            
+            for col in all_cols:
+                if col not in all_schema:
+                    validation_status = False
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+                else:
+                    validation_status = True
+                    with open(self.config.STATUS_FILE, 'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+
+            # Note: can also check the data types here.
+            # Note: a logger.info() here would be nice.
+
+            return validation_status
+        
+        except Exception as e:
+            raise e
