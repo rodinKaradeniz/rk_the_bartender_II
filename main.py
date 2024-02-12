8b08e839d30d40a250a4c48f21505e37b205608f
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2024-02-12 13:42:05 -0500

    Implemented data ingestion pipeline

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from rk_the_bartender import logger
+from rk_the_bartender.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Testing Logger (#1): Welcome to this custom log")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
