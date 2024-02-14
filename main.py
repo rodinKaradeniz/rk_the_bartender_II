bdbe28636c22eaeac911f8110ba51b3e3f0ad75c
rodinKaradeniz <mrodin.karadeniz@mail.utoronto.ca>
2024-02-14 11:51:31 -0500

    implemented model_trainer

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from rk_the_bartender import logger
 from rk_the_bartender.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from rk_the_bartender.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from rk_the_bartender.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from rk_the_bartender.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion"
@@ -32,6 +33,18 @@ STAGE_NAME = "Data Transformation"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Model Trainer"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainerTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
